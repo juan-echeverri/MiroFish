ab11ebf4b6afc76da26b44813cab8c0e35161ea3
Juan Echeverri <88244859+juan-echeverri@users.noreply.github.com>
2026-04-21 16:29:11 +0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,7 @@ server: {
   allowedHosts: true,
   proxy: {
       '/api': {
-        target: 'http://0.0.0.0:5001',
+        target: 'http://127.0.0.1:5001',
         changeOrigin: true,
         secure: false
       }
